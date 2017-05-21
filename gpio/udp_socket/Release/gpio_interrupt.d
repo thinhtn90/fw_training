e6a8c2dc11b983ea4a4bdabe4ee119bebdf2d4f2
Thinh Tn <thinh.tn90@gmail.com>
2017-05-21 13:48:17 +0700

    gpio interrupt
    Trainning of GPIO interrupt programming on CC3200

---
+++ gpio/udp_socket/Release/gpio_interrupt.d
@@ -0,0 +1,77 @@
+# FIXED
+
+gpio_interrupt.obj: ../gpio_interrupt.c
+gpio_interrupt.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdlib.h
+gpio_interrupt.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/linkage.h
+gpio_interrupt.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/string.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/simplelink.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../user.h
+gpio_interrupt.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/string.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../cc_pal.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/simplelink.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../source/objInclusion.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/trace.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/fs.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/socket.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/netapp.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/wlan.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/device.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/netcfg.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../source/nonos.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/wlan.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_ints.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_types.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_memmap.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom_map.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom_patch.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/interrupt.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/prcm.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/utils.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/uart.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/udma_if.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/common.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/gpio_if.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/pin.h
+gpio_interrupt.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/gpio.h
+
+../gpio_interrupt.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/string.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/simplelink.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../user.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/string.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../cc_pal.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/simplelink.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../source/objInclusion.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/trace.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/fs.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/socket.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/netapp.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/wlan.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/device.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/netcfg.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/../source/nonos.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/simplelink/include/wlan.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_ints.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_types.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_memmap.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom_map.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom_patch.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/interrupt.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/prcm.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/utils.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/uart.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/udma_if.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/common.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/gpio_if.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/pin.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/gpio.h: 
---
+++ gpio/udp_socket/gpio_interrupt.c
@@ -0,0 +1,34 @@
+/*
+ * gpio_interrupt.c
+ *
+ *  Created on: May 21, 2017
+ *      Author: illus_000
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+// simplelink includes
+#include "simplelink.h"
+#include "wlan.h"
+
+// driverlib includes
+#include "hw_ints.h"
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "interrupt.h"
+#include "prcm.h"
+#include "utils.h"
+#include "uart.h"
+
+// common interface includes
+#include "udma_if.h"
+#include "common.h"
+#ifndef NOTERM
+#include "uart_if.h"
+#endif
+#include "gpio_if.h"
+#include "pin.h"
+#include "gpio.h"
